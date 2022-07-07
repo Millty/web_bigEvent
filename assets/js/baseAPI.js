c4be22356a4cc76ddf4c0ee525643cbd444e3d0d
Millty <milkty@foxmail.com>
2022-07-07 13:59:34 +0800

    完成登录和注册的功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,7 +1,6 @@
-// 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
-// 会先调用 ajaxPrefilter 这个函数
-// 在这个函数中，可以拿到我们给Ajax提供的配置对象
-$.ajaxPrefilter(function(options) {
-  // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
-  options.url = 'http://ajax.frontend.itheima.net' + options.url
-})
+//每次调用$.get()或 $.post() 或$.ajax()时，会先调用ajaxPredilter函数
+//在该函数中可以拿到给ajax提供的配置对象
+$.ajaxPrefilter(function (option) {
+  //在发起真正的ajax请求之前，统一拼接请求的根路径
+  option.url = "http://127.0.0.1:3007" + option.url;
+});
