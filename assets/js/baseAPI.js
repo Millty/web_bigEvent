104e1ab40bf2a9ae97529eceaee5e5f9596d9829
Millty <milkty@foxmail.com>
2022-07-07 18:41:15 +0800

    完成主页功能开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,4 +3,25 @@
 $.ajaxPrefilter(function (option) {
   //在发起真正的ajax请求之前，统一拼接请求的根路径
   option.url = "http://127.0.0.1:3007" + option.url;
+
+  //统一为有权限的接口设置，设置headers请求头
+  if (option.url.indexOf("/my/") !== -1) {
+    option.headers = {
+      Authorization: localStorage.getItem("token") || "",
+    };
+  }
+
+  //全局统一挂载complete回调函数
+  option.complete = function (res) {
+    //在complete回调函数中，可以使用res.responseJSON拿到服务器想响应回来的数据
+    if (
+      res.responseJSON.status === 1 &&
+      res.responseJSON.message === "No authorization token was found"
+    ) {
+      //强制清空token
+      localStorage.removeItem("token");
+      //强制跳转登录页面
+      location.href = "/login.html";
+    }
+  };
 });
